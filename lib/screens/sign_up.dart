04c77a5fde3cfbdb465befd593b21566e35c2904
Tani21 <tanishkavaswani1@gmail.com>
2021-12-09 22:45:17 +0530

    otp removed from signup

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -114,6 +114,7 @@ bool _issecure2=true;
                         fontSize: 18,
                         fontWeight: FontWeight.bold),
                     keyboardType: TextInputType.name,
+                     cursorColor: Colors.black,
                     textInputAction: TextInputAction.next,
                   ),
                 ),
@@ -156,74 +157,75 @@ bool _issecure2=true;
                         fontSize: 18,
                         fontWeight: FontWeight.bold),
                     keyboardType: TextInputType.emailAddress,
+                     cursorColor: Colors.black,
                     textInputAction: TextInputAction.next,
                   ),
                 ),
               ),
 
-              SizedBox(
-                height: size.height * 0.02,
-              ),
+              // SizedBox(
+              //   height: size.height * 0.02,
+              // ),
 
-              Container(
-                height: size.height * 0.065,
-                width: size.width * 0.6,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(22),
-                  color: Colors.black87,
-                ),
-                child: TextButton(
-                  onPressed: ()  {
-                    Navigator.pushNamed(context, 'OtpVerification');
-                  },
-                  child: Text(
-                    'Send OTP',
-                    style: TextStyle(color: Colors.orange[300], fontSize: 20),
-                    //style: kBodyText.copyWith(fontWeight: FontWeight.bold)),
-                  ),
-                ),
-              ),
+              // Container(
+              //   height: size.height * 0.065,
+              //   width: size.width * 0.6,
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(22),
+              //     color: Colors.black87,
+              //   ),
+              //   child: TextButton(
+              //     onPressed: ()  {
+              //       Navigator.pushNamed(context, 'OtpVerification');
+              //     },
+              //     child: Text(
+              //       'Send OTP',
+              //       style: TextStyle(color: Colors.orange[300], fontSize: 20),
+              //       //style: kBodyText.copyWith(fontWeight: FontWeight.bold)),
+              //     ),
+              //   ),
+              // ),
 
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Container(
-                height: size.height * 0.065,
-                width: size.width * 0.8,
-                decoration: BoxDecoration(
-                  color: Colors.orange[300],
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Center(
-                  child: TextField(
-                    controller: _confpasswordController,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      //contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Icon(
-                          Icons.password_outlined,
-                          size: 28,
-                          color: Colors.black87,
-                        ),
-                      ),
-                      hintText: 'Enter OTP',
-                      hintStyle: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                    keyboardType: TextInputType.number,
-                    obscureText: true,
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-              ),
+              // SizedBox(
+              //   height: size.height * 0.02,
+              // ),
+              // Container(
+              //   height: size.height * 0.065,
+              //   width: size.width * 0.8,
+              //   decoration: BoxDecoration(
+              //     color: Colors.orange[300],
+              //     borderRadius: BorderRadius.circular(30),
+              //   ),
+              //   child: Center(
+              //     child: TextField(
+              //       controller: _confpasswordController,
+              //       decoration: InputDecoration(
+              //         border: InputBorder.none,
+              //         //contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              //         prefixIcon: Padding(
+              //           padding: const EdgeInsets.symmetric(horizontal: 20),
+              //           child: Icon(
+              //             Icons.password_outlined,
+              //             size: 28,
+              //             color: Colors.black87,
+              //           ),
+              //         ),
+              //         hintText: 'Enter OTP',
+              //         hintStyle: TextStyle(
+              //             color: Colors.black87,
+              //             fontSize: 18,
+              //             fontWeight: FontWeight.bold),
+              //       ),
+              //       style: TextStyle(
+              //           color: Colors.black87,
+              //           fontSize: 18,
+              //           fontWeight: FontWeight.bold),
+              //       keyboardType: TextInputType.number,
+              //       obscureText: true,
+              //       textInputAction: TextInputAction.next,
+              //     ),
+              //   ),
+              // ),
 
               SizedBox(
                 height: size.height * 0.02,
@@ -269,6 +271,7 @@ bool _issecure2=true;
                         fontWeight: FontWeight.bold),
                     keyboardType: TextInputType.emailAddress,
                     obscureText: _issecure,
+                     cursorColor: Colors.black,
                     textInputAction: TextInputAction.next,
                   ),
                 ),
@@ -318,6 +321,7 @@ bool _issecure2=true;
                         fontWeight: FontWeight.bold),
                     keyboardType: TextInputType.emailAddress,
                     obscureText: _issecure2,
+                    cursorColor: Colors.black,
                     textInputAction: TextInputAction.next,
                   ),
                 ),
